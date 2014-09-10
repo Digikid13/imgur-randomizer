6d46ce9d3ef0875427e793f18ec53ada96c1bf9c
Justin Cruz <digikid13@gmail.com>
2014-09-10 00:17:46 -0400

    created random image search based off query

--- imgur.js
+++ imgur.js
@@ -1,14 +1,34 @@
-$( document ).ready(function() {
-
+function ajaxCall(query) {
   $.ajax({
-      url: 'https://api.imgur.com/3/image/MkSBUPT',
+      url: 'https://api.imgur.com/3/gallery/search/',
       headers: {
           'Authorization': 'Client-ID 31852188e75d202'
       },
+      data: {q: JSON.stringify(query)},
       type: 'GET',
       success: function(data) {
-        document.write(data.data.link);
+        var random = Math.floor(Math.random() * data.data.length);
+
+        while(data.data[random].nsfw !== null) {
+          random = Math.floor(Math.random() * data.data.length);
+        }
+        
+        $('div#image').html('');
+        $('div#image').append('<img src="' + data.data[random].link + '" />');
       }
+  });
+}
+
+$( document ).ready(function() {
+  var $input = $('<input type="text"><br>');
+
+  $('body').prepend($input);
+
+  $("input").keyup(function (e) {
+    if (e.keyCode == 13) {
+      ajaxCall($(this).val());
+      $(this).val('');
+    }
   });
 
 });
